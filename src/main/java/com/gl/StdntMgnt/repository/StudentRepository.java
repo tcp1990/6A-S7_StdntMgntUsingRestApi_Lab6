fcc806f0bc13b34b3e909da11fbb001c1f12449d
prabhu <prabhu_tc@yahoo.com>
2022-11-02 11:50:02 +0530

    [Updating StudentRepository.java]

--- src/main/java/com/gl/StdntMgnt/repository/StudentRepository.java
+++ src/main/java/com/gl/StdntMgnt/repository/StudentRepository.java
@@ -1,7 +1,5 @@
 package com.gl.StdntMgnt.repository;
 
-import java.util.Set;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,5 +7,4 @@ import com.gl.StdntMgnt.model.Student;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Integer> {
-	Set<Student> findAllByFirstName(String firstName);
 }
