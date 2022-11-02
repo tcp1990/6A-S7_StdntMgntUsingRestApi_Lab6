766dcf0b112900e9f9a9ac7288eca53481f399a0
prabhu <prabhu_tc@yahoo.com>
2022-11-02 11:55:23 +0530

    [Updating scripts.sql]

--- scripts.sql
+++ scripts.sql
@@ -8,7 +8,7 @@ CREATE DATABASE IF NOT EXISTS `college-fest`;
 USE `college-fest`;
 
 /*Creating students table*/
-CREATE TABLE IF NOT EXISTS student(
+CREATE TABLE IF NOT EXISTS students(
 id int primary key NOT NULL AUTO_INCREMENT,
 name varchar(255) NOT NULL,
 department varchar(255),
@@ -16,10 +16,10 @@ country varchar(255) NOT NULL
 );
 
 /*Inserting into students table*/
-INSERT INTO student(name, department, country) VALUES("Suresh","B.Tech",'India');
-INSERT INTO student(name, department, country) VALUES("Muri","B.Arch",'Canada');
-INSERT INTO student(name, department, country) VALUES("Daniel","B.Tech",'New Zealand');
-INSERT INTO student(name, department, country) VALUES("Tanya","B.Com",'USA');
+INSERT INTO students(name, department, country) VALUES("Suresh","B.Tech",'India');
+INSERT INTO students(name, department, country) VALUES("Muri","B.Arch",'Canada');
+INSERT INTO students(name, department, country) VALUES("Daniel","B.Tech",'New Zealand');
+INSERT INTO students(name, department, country) VALUES("Tanya","B.Com",'USA');
 
 /*Creating roles table*/
 CREATE TABLE IF NOT EXISTS roles(
