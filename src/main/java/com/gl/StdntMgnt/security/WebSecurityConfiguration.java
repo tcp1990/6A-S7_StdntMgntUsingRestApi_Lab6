1ec094aaf5c2d55e54f14e2940fe9155a6fbbf6a
prabhu <prabhu_tc@yahoo.com>
2022-11-03 15:37:12 +0530

    [Updating WebSecurityConfiguration.java]

--- src/main/java/com/gl/StdntMgnt/security/WebSecurityConfiguration.java
+++ src/main/java/com/gl/StdntMgnt/security/WebSecurityConfiguration.java
@@ -1,22 +1,30 @@
 package com.gl.StdntMgnt.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity
 @Configuration
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		BCryptPasswordEncoder encoder = passwordEncoder();
 		auth.inMemoryAuthentication().withUser("user") // #1
-				.password("password").roles("USER").and().withUser("admin") // #2
-				.password("password").roles("ADMIN", "USER");
+				.password(encoder.encode("password")).roles("USER").and().withUser("admin") // #2
+				.password(encoder.encode("password")).roles("ADMIN", "USER");
 	}
 
 	@Override
