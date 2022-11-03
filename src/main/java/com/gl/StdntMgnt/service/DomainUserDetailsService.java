373220af67ab1c59534c2ff5e69e5318e0b8c9ca
prabhu <prabhu_tc@yahoo.com>
2022-11-03 19:09:46 +0530

    [Updating DomainUserDetailsService.java]

--- src/main/java/com/gl/StdntMgnt/service/DomainUserDetailsService.java
+++ src/main/java/com/gl/StdntMgnt/service/DomainUserDetailsService.java
@@ -1,5 +1,10 @@
 package com.gl.StdntMgnt.service;
 
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Criterion;
+import org.hibernate.criterion.Restrictions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -7,6 +12,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.gl.StdntMgnt.model.DomainUserDetails;
+import com.gl.StdntMgnt.model.Student;
 import com.gl.StdntMgnt.model.User;
 import com.gl.StdntMgnt.repository.UserRepository;
 
@@ -14,15 +20,25 @@ import com.gl.StdntMgnt.repository.UserRepository;
 public class DomainUserDetailsService implements UserDetailsService {
 
 	@Autowired
-	private UserRepository userRepository;
+	SessionFactory factory;
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		User user = this.userRepository.findByUsername(username)
-				.orElseThrow(() -> new UsernameNotFoundException("Invalid User"));
-		System.out.println("User from the repository ");
-		System.out.println(user);
-		// convert into UserDetails object which Spring Security can understand
-		return new DomainUserDetails(user);
+
+		Session session = factory.getCurrentSession();
+		Criteria c = session.createCriteria(User.class);
+		Criterion n = Restrictions.eq("username", username);
+		c.add(n);
+		var userList = c.list();
+		if (!userList.isEmpty()) {
+			User user = (User) userList.get(0);
+			System.out.println("User from the repository ");
+			System.out.println(user);
+			// convert into UserDetails object which Spring Security can understand
+			return new DomainUserDetails(user);
+		} else {
+			throw new UsernameNotFoundException("Invalid User");
+		}
+
 	}
 }
