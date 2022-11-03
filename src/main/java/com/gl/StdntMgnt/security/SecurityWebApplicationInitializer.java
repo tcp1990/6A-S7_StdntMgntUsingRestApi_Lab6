4b1768cf34483823b7e0eb0ddff8972c6ee6f532
prabhu <prabhu_tc@yahoo.com>
2022-11-03 14:46:44 +0530

    [Adding security files]

---
+++ src/main/java/com/gl/StdntMgnt/security/SecurityWebApplicationInitializer.java
@@ -0,0 +1,6 @@
+package com.gl.StdntMgnt.security;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+}
