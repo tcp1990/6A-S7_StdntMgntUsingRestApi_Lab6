fe573fa883b11af1a908c2b5414324aca8897017
prabhu <prabhu_tc@yahoo.com>
2022-11-03 15:21:13 +0530

    [Updating WebSecurityConfiguration.java]

--- src/main/java/com/gl/StdntMgnt/security/WebSecurityConfiguration.java
+++ src/main/java/com/gl/StdntMgnt/security/WebSecurityConfiguration.java
@@ -1,56 +1,35 @@
 package com.gl.StdntMgnt.security;
 
-import static org.springframework.http.HttpMethod.DELETE;
-import static org.springframework.http.HttpMethod.POST;
-import static org.springframework.http.HttpMethod.PUT;
-import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
-
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
+@Configuration
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
-	
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
-    	// configure authorization rules here
- 		httpSecurity.cors().disable();
- 		httpSecurity.csrf().disable();
- 		httpSecurity.headers().frameOptions().disable();
- 		httpSecurity.authorizeRequests().antMatchers(POST, "/api/employees/**").hasRole("ADMIN");
- 		httpSecurity.authorizeRequests().antMatchers(PUT, "/api/employees/**",  "/api/roles/**", "/api/users/**").denyAll();
- 		httpSecurity.authorizeRequests().antMatchers(DELETE, "/api/roles/**", "/api/users/**").denyAll();
- 		//httpSecurity.authorizeRequests().antMatchers("/h2-console/**", "/login**", "/contact-us**").permitAll();
- 		httpSecurity.authorizeRequests().antMatchers("/**").permitAll();
- 		httpSecurity.authorizeRequests().anyRequest().authenticated().and().formLogin().and().httpBasic()
- 				/*
- 				 * Set the session creation policy to avoid using cookies for authentication
- 				 * https://stackoverflow.com/questions/50842258/spring-security-caching-my-
- 				 * authentication/50847571
- 				 */
- 				.and().sessionManagement().sessionCreationPolicy(STATELESS);
- 		
-        return httpSecurity.build();
-    }
-    
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
-        return authenticationConfiguration.getAuthenticationManager();
-    }
 
-    @Bean
-    public PasswordEncoder bcryptPasswordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser("user") // #1
+				.password("password").roles("USER").and().withUser("admin") // #2
+				.password("password").roles("ADMIN", "USER");
+	}
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/resources/**"); // #3
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers("/signup", "/about").permitAll() // #4
+				.antMatchers("/admin/**").hasRole("ADMIN") // #6
+				.anyRequest().authenticated() // 7
+				.and().formLogin() // #8
+				.permitAll(); // #5
+	}
 }
